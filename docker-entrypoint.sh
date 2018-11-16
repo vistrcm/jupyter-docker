35c6ad6c4933caded32732b634db23d22e083cf8
Stanislav Vitko <stas@vitko.me>
2018-11-15 16:20:38 -0800

    now bash is really the shell for a user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jupyter notebook --no-browser --ip=0.0.0.0 --NotebookApp.password=${NOTEBOOK_PASSWORD}
+jupyter notebook --no-browser --ip=0.0.0.0 --NotebookApp.password=${NOTEBOOK_PASSWORD} --NotebookApp.terminado_settings='{"shell_command": ["bash"]}'
